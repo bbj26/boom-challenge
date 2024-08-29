6a89197b9902bf1219efbf63940d8dafb1f4b8ce
Branimir Corluka <brane.corluka@gmail.com>
2024-08-29 18:05:18 +0200

    add persisted state and gameSlice

---
+++ boom-challenge/src/constants/gameConfigConstants.ts
@@ -0,0 +1,6 @@
+export const SMILEY_ICON = '😃';
+export const BOMB_ICON = '💥';
+export const RESETS_ICON = '🌀';
+
+export const NUMBER_OF_CONSECUTIVE_SMILEYS_TO_WIN_GAME = 3;
+export const NUMBER_OF_CONSECUTIVE_BOMBS_TO_LOSE_GAME = 2;
